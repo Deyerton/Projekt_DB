a6414b7c1fb6e52014c684fa3d1d2570075e9be5
Deyerton <64000964+Deyerton@users.noreply.github.com>
2020-04-20 10:58:39 +0200

    Update builder.cpp

--- builder.cpp
+++ builder.cpp
@@ -1,9 +1,3 @@
-/****************************************
-*   Wzorzec Projektowy Builder          *
-*   (budowniczy)                        *  
-*   www.algorytm.org                    *
-*   Opracowal Dworak Kamil              *
-*****************************************/
 #include <stdafx.h> 
 #include <iostream>
 #include <string>
